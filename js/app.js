158263bb563349a582b1a05c6da95d49adbf54a3
holger.boetticher <holger.boetticher@arcor.de>
2019-03-12 22:50:49 +0100

    feat: first draft for the figure movements and setup for GameStart

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+let Enemy = function(initX = 0,initY = 0, maxSpeed = 50, minSpeed = 50) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.startX = initX;
+    this.startY = initY;
+    this.x = initX;
+    this.y = initY;
+    this.maxSpeed = maxSpeed;
+    this.minSpeed = minSpeed;
+    this.speed = Math.floor((Math.random() * this.maxSpeed) +  this.minSpeed);
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -14,6 +21,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if (this.x > 500) {
+      this.speed = Math.floor((Math.random() * this.maxSpeed) + this.minSpeed);
+      this.x = this.startX - this.speed;
+    }
+    this.x = this.x + (this.speed*dt);
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +37,47 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+let Player = function (initX = 200,initY = 405,initSpeedX = 100,initSpeedY = 84) {
+  this.sprite = 'images/char-boy.png';
+  this.x = initX;
+  this.y = initY;
+  this.startX = initX;
+  this.startY = initY;
+  this.speedX = initSpeedX;
+  this.speedY = initSpeedY;
+  Player.prototype.render = function() {
+      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  };
+  Player.prototype.update = function() {
+      return 0;
+  };
+  Player.prototype.handleInput = function(key) {
+    switch (key) {
+      case 'left':
+          this.x = this.x - this.speedX;
+        break;
+      case 'up':
+          this.y = this.y - this.speedY;
+        break;
+      case 'right':
+          this.x = this.x + this.speedX;
+        break;
+      case 'down':
+          this.y = this.y + this.speedY;
+        break;
+    }
+    console.log(this.x,this.y);
+  };
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+let allEnemies = [new Enemy(-20,60,200,100),
+                  new Enemy(-20,143,150,75),
+                  new Enemy(-20,227,125,75)];
+let player = new Player();
 
 
 // This listens for key presses and sends the keys to your
