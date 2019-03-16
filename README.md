e27fabf1b37d7a026f3b25423243de13f01da33e
holger.boetticher <holger.boetticher@arcor.de>
2019-03-16 21:47:52 +0100

    style: butify code

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
 # Classic Arcade Game Clone Project
 
 ## Descrition
+
 This game is part of the Udacity Project for the completion of the Froned Web Development Nanodegree. The game is developed using HTML, CSS and pure JavaScript.
-The game it self is a slim remake of the arcade game *Frogger* from the '80's.
+The game it self is a slim remake of the arcade game _Frogger_ from the '80's.
 
 ## Instructions
 
 ### Installation of the game
+
 Simply download the repo and open index.html
 
 ### How to play
+
 The Goal of the game is to get your figure over the river on top of the gamebord.
 To do this you have to move your figure with the arrow keys arround and collect the dimonds untill you find a key. After that you can cross the river.
 
@@ -20,15 +23,16 @@ To do this you have to move your figure with the arrow keys arround and collect 
 #####key: 60
 
 ## Acknowledgements
+
 The following seits and resources where used to develop or inspire parts of this program:
 
-- Mentors on the Udacity Classroom
-- Fellow course mates
-- instructions on how to get started https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true
-- for self-checking of my submission. [rubric](https://review.udacity.com/#!/rubrics/15/view)
-- The collision check logic validation is adaptet from  the version of Antonella Dean'scollision check: Copyright (c) 2018 Antonella Bernobich Dean;
---(https://github.com/aberdean/google-scholarship-fend-projects/tree/master/classic-arcade-game-clone)
-
+-   Mentors on the Udacity Classroom
+-   Fellow course mates
+-   instructions on how to get started <https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true>
+-   for self-checking of my submission. [rubric](https://review.udacity.com/#!/rubrics/15/view)
+-   The collision check logic validation is adaptet from  the version of Antonella Dean'scollision check: Copyright (c) 2018 Antonella Bernobich Dean;
+    \--(<https://github.com/aberdean/google-scholarship-fend-projects/tree/master/classic-arcade-game-clone>)
 
 ## Author
-- Holger Bötticher
+
+-   Holger Bötticher
--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 /**
-* @description Represents a movable object in the game, it contains the
-*              positioning, the image and the main drawing functions
-* @constructor
-* @param {int} startX - Startposition for the x (width) line
-* @param {int} startY - Startposition for the y (hight) line
-* @param {string} spirit - picture that will be drawn for the object
-*                          default is "images/enemy-bug.png"
-*/
+ * @description Represents a movable object in the game, it contains the
+ *              positioning, the image and the main drawing functions
+ * @constructor
+ * @param {int} startX - Startposition for the x (width) line
+ * @param {int} startY - Startposition for the y (hight) line
+ * @param {string} spirit - picture that will be drawn for the object
+ *                          default is "images/enemy-bug.png"
+ */
 class GameObject {
-  constructor(startX = 0,startY = 0, sprite = '') {
+  constructor(startX = 0, startY = 0, sprite = '') {
     this.startX = startX;
     this.startY = startY;
     this.x = startX;
@@ -16,75 +16,75 @@ class GameObject {
     this.sprite = sprite;
   }
   /**
-  * @description Draw the enemy on the screen, required method for game
-  */
+   * @description Draw the enemy on the screen, required method for game
+   */
   render() {
-      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
   /**
-  * @description  Checks if the object collides with a an nother game object
-  * @remark       collision logic addaptet from
-  *               https://stackoverflow.com/questions/13916966/adding-collision-detection-to-images-drawn-on-canvas
-  * @return returns the element that the object colides with
-  */
+   * @description  Checks if the object collides with a an nother game object
+   * @remark       collision logic addaptet from
+   *               https://stackoverflow.com/questions/13916966/adding-collision-detection-to-images-drawn-on-canvas
+   * @return returns the element that the object colides with
+   */
   checkCollisions() {
-    for (let elemet of allCollisionsObjects ) {
+    for (let elemet of allCollisionsObjects) {
       /* The collision logic validation is adaptet from  the version of Antonella Dean's
-   		 * collision check: Copyright (c) 2018 Antonella Bernobich Dean;
-  		 * https://github.com/aberdean/google-scholarship-fend-projects/tree/master/classic-arcade-game-clone */
-       if (elemet != this && (Math.abs(player.x - elemet.x) <= 60) && (Math.abs(player.y - elemet.y) <= 17)) {
-          return elemet;
-       }
+       * collision check: Copyright (c) 2018 Antonella Bernobich Dean;
+       * https://github.com/aberdean/google-scholarship-fend-projects/tree/master/classic-arcade-game-clone */
+      if (elemet != this && (Math.abs(player.x - elemet.x) <= 60) && (Math.abs(player.y - elemet.y) <= 17)) {
+        return elemet;
+      }
     }
     return null;
   }
 }
 
 /**
-* @description  Represents an enemy object in the game, it holds the
-*               positioning, the image, the main drawing functions and also
-*               the function for the movement of the object.
-* @remark       the movment speed will be generatet randomly, in the number
-*               range that is defined with maxSpeed and minSpeed
-* @constructor
-* @param {int} startX - Startposition for the x (width) line
-* @param {int} startY - Startposition for the y (hight) line
-* @param {int} minSpeed - the min Pixel value that the element will move
-* @param {int} maxSpeed - the max Pixel value that the element will move
-*/
+ * @description  Represents an enemy object in the game, it holds the
+ *               positioning, the image, the main drawing functions and also
+ *               the function for the movement of the object.
+ * @remark       the movment speed will be generatet randomly, in the number
+ *               range that is defined with maxSpeed and minSpeed
+ * @constructor
+ * @param {int} startX - Startposition for the x (width) line
+ * @param {int} startY - Startposition for the y (hight) line
+ * @param {int} minSpeed - the min Pixel value that the element will move
+ * @param {int} maxSpeed - the max Pixel value that the element will move
+ */
 class Enemy extends GameObject {
-  constructor(startX = 0, startY = 0, minSpeed = 50, maxSpeed = 50,sprite = 'images/enemy-bug.png') {
-    super(startX,startY,sprite);
+  constructor(startX = 0, startY = 0, minSpeed = 50, maxSpeed = 50, sprite = 'images/enemy-bug.png') {
+    super(startX, startY, sprite);
     this.maxSpeed = maxSpeed;
     this.minSpeed = minSpeed;
-    this.speed = Math.floor((Math.random() * this.maxSpeed) +  this.minSpeed);
+    this.speed = Math.floor((Math.random() * this.maxSpeed) + this.minSpeed);
   }
 
   /**
-  * @description  Update the enemy's position, required method for game
-  * @param {int} dt - a time delta between ticks
-  *                   speed for all computers.
-  */
+   * @description  Update the enemy's position, required method for game
+   * @param {int} dt - a time delta between ticks
+   *                   speed for all computers.
+   */
   update(dt) {
-      if (this.x > 500) {
-        this.speed = Math.floor((Math.random() * this.maxSpeed) + this.minSpeed);
-        this.x = this.startX - this.speed;
-      }
-      this.x = this.x + (this.speed*dt);
+    if (this.x > 500) {
+      this.speed = Math.floor((Math.random() * this.maxSpeed) + this.minSpeed);
+      this.x = this.startX - this.speed;
+    }
+    this.x = this.x + (this.speed * dt);
   }
 }
 
 /**
-* @description  Represents a gem object in the game, it holds the
-*               positioning, the image, the main drawing functions.
-* @constructor
-* @param {int} startX - Startposition for the x (width) line
-* @param {int} startY - Startposition for the y (hight) line
-* @param {int} color - color of the gem (1 blue, 2 green, 3 orange);
-*/
+ * @description  Represents a gem object in the game, it holds the
+ *               positioning, the image, the main drawing functions.
+ * @constructor
+ * @param {int} startX - Startposition for the x (width) line
+ * @param {int} startY - Startposition for the y (hight) line
+ * @param {int} color - color of the gem (1 blue, 2 green, 3 orange);
+ */
 class Gem extends GameObject {
   constructor(startX = 0, startY = 0, color = 1) {
-    super(startX,startY);
+    super(startX, startY);
     this.points = 10;
     this.isKey = false;
     switch (color) {
@@ -96,13 +96,13 @@ class Gem extends GameObject {
         break;
       case 4:
       case 5:
-          this.sprite = 'images/Gem Green.png';
-          this.points = 40;
+        this.sprite = 'images/Gem Green.png';
+        this.points = 40;
         break;
       case 6:
-          this.sprite = 'images/Key.png';
-          this.isKey = true;
-          this.points = 60;
+        this.sprite = 'images/Key.png';
+        this.isKey = true;
+        this.points = 60;
         break;
       default:
         this.sprite = 'images/Gem Orange.png';
@@ -111,42 +111,42 @@ class Gem extends GameObject {
   }
 
   /**
-  * @description  could update the movment of an gem
-  * @param {int} dt - a time delta between ticks
-  *                   speed for all computers.
-  */
+   * @description  could update the movment of an gem
+   * @param {int} dt - a time delta between ticks
+   *                   speed for all computers.
+   */
   update(dt) {
 
   }
   /**
-  * @description  staic method to generate a new gem
-  * @param {int} dt - a time delta between ticks
-  *                   speed for all computers.
-  */
-  static getNewGem(){
+   * @description  staic method to generate a new gem
+   * @param {int} dt - a time delta between ticks
+   *                   speed for all computers.
+   */
+  static getNewGem() {
     const x = Math.floor((Math.random() * 3) + 1) * 100;
     const y = Math.floor((Math.random() * 3) + 1) * 76;
-    let returnGem = new Gem(x,y,Math.floor((Math.random() * 12) + 1))
+    let returnGem = new Gem(x, y, Math.floor((Math.random() * 12) + 1));
     return returnGem;
   }
 }
 
 
 /**
-* @description  Represents a player object in the game, it holds the
-*               positioning, the image, the main drawing functions and also
-*               the function for the movement of the player with the key events.
-* @remark       the movment speed will be generatet randomly, in the number
-*               range that is defined with maxSpeed and minSpeed
-* @constructor
-* @param {int} startX - Startposition for the x (width) line
-* @param {int} startY - Startposition for the y (hight) line
-* @param {int} minSpeed - the min Pixel value that the element will move
-* @param {int} maxSpeed - the max Pixel value that the element will move
-*/
+ * @description  Represents a player object in the game, it holds the
+ *               positioning, the image, the main drawing functions and also
+ *               the function for the movement of the player with the key events.
+ * @remark       the movment speed will be generatet randomly, in the number
+ *               range that is defined with maxSpeed and minSpeed
+ * @constructor
+ * @param {int} startX - Startposition for the x (width) line
+ * @param {int} startY - Startposition for the y (hight) line
+ * @param {int} minSpeed - the min Pixel value that the element will move
+ * @param {int} maxSpeed - the max Pixel value that the element will move
+ */
 class Player extends GameObject {
-  constructor(startX = 0, startY = 0, movementX = 100, movementY = 84,sprite = 'images/char-boy.png') {
-    super(startX,startY,sprite);
+  constructor(startX = 0, startY = 0, movementX = 100, movementY = 84, sprite = 'images/char-boy.png') {
+    super(startX, startY, sprite);
     this.speedX = movementX;
     this.speedY = movementY;
     this.points = 0;
@@ -157,32 +157,31 @@ class Player extends GameObject {
   }
 
   /**
-  * @description  Update the enemy's position, required method for game
-  * @param {int} dt - a time delta between ticks
-  *                   speed for all computers.
-  */
+   * @description  Update the enemy's position, required method for game
+   * @param {int} dt - a time delta between ticks
+   *                   speed for all computers.
+   */
   update(dt) {
     let cObject = super.checkCollisions();
     if (cObject != null) {
-        if (cObject instanceof Gem) {
-            if (cObject.isKey) {
-              this.hasKey = true;
-            }
-            this.points += cObject.points;
-            allCollisionsObjects.delete(cObject);
-            allCollisionsObjects.add(Gem.getNewGem());
-        }else{
-          this.live -=1;
-          live.innerHTML = this.live;
-          console.log(this.live);
-          if (this.live <= 0 ) {
-            this.dead = true;
-            this.live = 3;
-          }
-          else {
-            this.resetPlayer();
-          }
+      if (cObject instanceof Gem) {
+        if (cObject.isKey) {
+          this.hasKey = true;
         }
+        this.points += cObject.points;
+        allCollisionsObjects.delete(cObject);
+        allCollisionsObjects.add(Gem.getNewGem());
+      } else {
+        this.live -= 1;
+        live.innerHTML = this.live;
+        console.log(this.live);
+        if (this.live <= 0) {
+          this.dead = true;
+          this.live = 3;
+        } else {
+          this.resetPlayer();
+        }
+      }
     }
     score.innerHTML = this.points;
     live.innerHTML = this.live;
@@ -190,43 +189,42 @@ class Player extends GameObject {
 
 
   /**
-  * @description  Sets the gaming figure back to start
-  */
-  resetPlayer(){
+   * @description  Sets the gaming figure back to start
+   */
+  resetPlayer() {
     this.x = this.startX;
     this.y = this.startY;
   }
 
   /**
-  * @description  displays the win screen
-  */
-  win(){
+   * @description  displays the win screen
+   */
+  win() {
     this.won = true;
   }
 
   /**
-  * @description  handles the movement of the player figuer
-  * @param {string} key - the key that the player pressd for the movement
-  */
+   * @description  handles the movement of the player figuer
+   * @param {string} key - the key that the player pressd for the movement
+   */
   handleInput(key) {
     switch (key) {
       case 'left':
-          if (this.x - this.speedX >= 0) {
-            this.x = this.x - this.speedX;
-          }
+        if (this.x - this.speedX >= 0) {
+          this.x = this.x - this.speedX;
+        }
         break;
       case 'up':
-          if (this.y - this.speedY >= 0) {
-            this.y = this.y - this.speedY;
-          }
-          else if(this.hasKey) {
-            this.win();
-          }
+        if (this.y - this.speedY >= 0) {
+          this.y = this.y - this.speedY;
+        } else if (this.hasKey) {
+          this.win();
+        }
         break;
       case 'right':
-          if (this.x + this.speedX <= 400) {
-            this.x = this.x + this.speedX;
-          }
+        if (this.x + this.speedX <= 400) {
+          this.x = this.x + this.speedX;
+        }
         break;
       case 'down':
         if (this.y + this.speedY <= 405) {
@@ -241,47 +239,50 @@ class Player extends GameObject {
 // Now instantiate your objects.
 const score = document.querySelector('#scores');
 const live = document.querySelector('#liveCounter');
-let allCollisionsObjects = new Set([new Enemy(-20,60,100,200),
-                  new Enemy(-20,143,75,150),
-                  new Enemy(-20,227,75,125)]);
-let displayOnlyObjects = new Set([new GameObject(0,-18,'images/Rock.png'),
-                                  new GameObject(100,-18,'images/Rock.png'),
-                                  new GameObject(200,-18,'images/Rock.png'),
-                                  new GameObject(300,-18,'images/Rock.png'),
-                                  new GameObject(400,-18,'images/Rock.png')]);
+let allCollisionsObjects = new Set([new Enemy(-20, 60, 100, 200),
+  new Enemy(-20, 143, 75, 150),
+  new Enemy(-20, 227, 75, 125)
+]);
+let displayOnlyObjects = new Set([new GameObject(0, -18, 'images/Rock.png'),
+  new GameObject(100, -18, 'images/Rock.png'),
+  new GameObject(200, -18, 'images/Rock.png'),
+  new GameObject(300, -18, 'images/Rock.png'),
+  new GameObject(400, -18, 'images/Rock.png')
+]);
 // player object in a variable called player
-let player = new Player(200,405,100,84,'images/char-boy.png');
+let player = new Player(200, 405, 100, 84, 'images/char-boy.png');
 allCollisionsObjects.add(player);
 allCollisionsObjects.add(Gem.getNewGem());
 
 
 /**
-* @description  This listens for key presses and sends the keys to your
-*               Player.handleInput() method. You don't need to modify this.
-*/
+ * @description  This listens for key presses and sends the keys to your
+ *               Player.handleInput() method. You don't need to modify this.
+ */
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
 
 /**
-* @description  eventliserener for the button in the messagebox resets the game
-*/
+ * @description  eventliserener for the button in the messagebox resets the game
+ */
 document.querySelector('.button').addEventListener('click', function(e) {
   gameBord.classList.remove('hide');
   gameInfo.classList.remove('hide');
   gameMessageBox.classList.add('hide');
-  allCollisionsObjects = new Set([new Enemy(-20,60,100,200),
-                    new Enemy(-20,143,75,150),
-                    new Enemy(-20,227,75,125)]);
+  allCollisionsObjects = new Set([new Enemy(-20, 60, 100, 200),
+    new Enemy(-20, 143, 75, 150),
+    new Enemy(-20, 227, 75, 125)
+  ]);
   // player object in a variable called player
-  player = new Player(200,405,100,84,'images/char-boy.png');
+  player = new Player(200, 405, 100, 84, 'images/char-boy.png');
   allCollisionsObjects.add(player);
   allCollisionsObjects.add(Gem.getNewGem());
 });
